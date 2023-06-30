c70b67a1b4cde76b2c534022531d32c666fefe77
KathyaGonzalez <gonzco32@gmail.com>
2023-06-29 22:59:36 -0600

    Web

--- src/main/java/kathya/bilena/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/kathya/bilena/springbootbackend/SpringbootBackendApplication.java
@@ -12,9 +12,77 @@ import java.net.URL;
 import java.util.Scanner;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 @SpringBootApplication
 public class SpringbootBackendApplication extends Thread implements CommandLineRunner{
+
+	@GetMapping("/MostrarDatos")
+	public String getMessage() {
+		String mensaje = "";
+		for (int i = 0; i < usuarioController.getAllUsuarios().size(); i++){
+			String nombre = usuarioController.getAllUsuarios().get(i).getNombre();
+			String espacio = "          ";
+			if (nombre.length() < 20) {
+				for (int j = 0; j < (20 - nombre.length()); j++){
+					espacio += " ";
+				}
+			}
+			String correo = usuarioController.getAllUsuarios().get(i).getCorreo();
+			String espacioCorreo = "          ";
+			if (correo.length() < 30) {
+				for (int j = 0; j < (30 - correo.length()); j++){
+					espacioCorreo += " ";
+				}
+			}
+			mensaje += nombre + espacio + correo + espacioCorreo + usuarioController.getAllUsuarios().get(i).getEmpleo() + "\n";
+		}
+		return mensaje;
+	}
+
+	@GetMapping("/CargarDatos")
+	public String getMessag() {
+		try{
+			int peticiones = 4;
+			int ronda = 1;
+			while (ronda > 0){
+				ronda -= 1;
+				URL url = new URL("https://random-data-api.com/api/v2/users?size=" + peticiones);
+				HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.connect();
+				int responseCode = conn.getResponseCode();
+				if (responseCode != 200){
+					throw new RuntimeException("Ocurrió un error: "+ responseCode);
+				}else{
+					StringBuilder informationString = new StringBuilder();
+					Scanner scanner = new Scanner(url.openStream());
+					while(scanner.hasNext()){
+						informationString.append(scanner.nextLine());
+					}
+					scanner.close();
+					JSONArray jsonArray = new JSONArray(informationString.toString());
+					for(int x=0; x< peticiones; x++){
+						JSONObject jsonObject = jsonArray.getJSONObject(x);
+						String nombre = jsonObject.getString("first_name") + " " + jsonObject.getString("last_name");
+						String correo = jsonObject.getString("email");
+						String empleo = jsonObject.getJSONObject("employment").getString("title");
+						Usuario usuario = new Usuario();
+						usuario.setNombre(nombre);
+						usuario.setCorreo(correo);
+						usuario.setEmpleo(empleo);
+						usuarioRepository.save(usuario);
+						Thread.sleep(500);
+					}
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "Cargando datos";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
@@ -27,94 +95,5 @@ public class SpringbootBackendApplication extends Thread implements CommandLineR
 	private UsuarioController usuarioController;
 	@Override
 	public void run(String... args) throws Exception{
-		int opcion = 1;
-		while (opcion!= 3) {
-			System.out.println("------------------------------------------------------------------------------------------------------");
-			System.out.println("                                               Menú");
-			System.out.println("------------------------------------------------------------------------------------------------------");
-			System.out.println("1. Cargar datos");
-			System.out.println("2. Mostrar datos");
-			System.out.println("3. Salir");
-			Scanner entrada = new Scanner(System.in);
-			System.out.printf("Ingrese la opción que desea ejecutar: ");
-			try {
-				opcion = entrada.nextInt();
-			}catch (Exception e) {
-				System.exit(0);
-			}
-			if (opcion == 1) {
-				System.out.println("------------------------------------------------------------------------------------------------------");
-				System.out.println("                                             CARGAR DATOS");
-				System.out.println("------------------------------------------------------------------------------------------------------");
-				try{
-					int peticiones = 4;
-					int ronda = 1;
-					while (ronda > 0){
-						ronda -= 1;
-						URL url = new URL("https://random-data-api.com/api/v2/users?size=" + peticiones);
-						HttpURLConnection conn= (HttpURLConnection) url.openConnection();
-						conn.setRequestMethod("GET");
-						conn.connect();
-						int responseCode = conn.getResponseCode();
-						if (responseCode != 200){
-							throw new RuntimeException("Ocurrió un error: "+ responseCode);
-						}else{
-							StringBuilder informationString = new StringBuilder();
-							Scanner scanner = new Scanner(url.openStream());
-							while(scanner.hasNext()){
-								informationString.append(scanner.nextLine());
-							}
-							scanner.close();
-							JSONArray jsonArray = new JSONArray(informationString.toString());
-							for(int x=0; x< peticiones; x++){
-								JSONObject jsonObject = jsonArray.getJSONObject(x);
-								String nombre = jsonObject.getString("first_name") + " " + jsonObject.getString("last_name");
-								System.out.println("nombre: " + nombre);
-								String correo = jsonObject.getString("email");
-								System.out.println("correo: " + correo);
-								String empleo = jsonObject.getJSONObject("employment").getString("title");
-								System.out.println("empleo: " + empleo);
-								Usuario usuario = new Usuario();
-								usuario.setNombre(nombre);
-								usuario.setCorreo(correo);
-								usuario.setEmpleo(empleo);
-								usuarioRepository.save(usuario);
-								Thread.sleep(500);
-								System.out.println("------------------------------------------------------------------------------------------------------");
-							}
-						}
-					}
-				}catch(Exception e){
-					e.printStackTrace();
-				}
-			} else if (opcion == 2) {
-				System.out.println("------------------------------------------------------------------------------------------------------");
-				System.out.println("                                               MOSTRAR DATOS");
-				System.out.println("------------------------------------------------------------------------------------------------------");
-				System.out.println("          Nombre                     Correo                              Empleo          ");
-				for (int i = 0; i < usuarioController.getAllUsuarios().size(); i++){
-					String nombre = usuarioController.getAllUsuarios().get(i).getNombre();
-					String espacio = "          ";
-					if (nombre.length() < 20) {
-							for (int j = 0; j < (20 - nombre.length()); j++){
-								espacio += " ";
-							}
-					}
-					String correo = usuarioController.getAllUsuarios().get(i).getCorreo();
-					String espacioCorreo = "          ";
-					if (correo.length() < 30) {
-						for (int j = 0; j < (30 - correo.length()); j++){
-							espacioCorreo += " ";
-						}
-					}
-					System.out.println(nombre + espacio + correo + espacioCorreo + usuarioController.getAllUsuarios().get(i).getEmpleo());
-				}
-			} else {
-				System.out.println("------------------------------------------------------------------------------------------------------");
-				System.out.println("                                             SALIR");
-				System.out.println("------------------------------------------------------------------------------------------------------");
-				System.exit(0);
-			}
-		}
 	}
 }
